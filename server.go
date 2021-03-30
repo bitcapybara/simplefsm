20ee443d9109c887d44e2735e81093addf6fcd9b
bitcapybara <1227686398@qq.com>
2021-03-30 21:34:59 +0800

    添加命令行参数，完善main函数

--- server.go
+++ server.go
@@ -3,15 +3,41 @@ package main
 import (
 	"bytes"
 	"encoding/gob"
+	"flag"
 	"fmt"
 	"github.com/bitcapybara/raft"
 	"github.com/bitcapybara/simplefsm/raftimpl"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"strings"
 )
 
 func main() {
-	s := newServer("", nil)
+	// 命令行参数定义
+	var me string
+	flag.StringVar(&me,"me", "", "当前节点nodeId")
+	var peerStr string
+	flag.StringVar(&peerStr, "peers", "", "指定所有节点地址，nodeId@nodeAddr，多个地址使用逗号间隔")
+	flag.Parse()
+
+	// 命令行参数解析
+	if me == "" {
+		log.Fatal("未指定当前节点id！")
+	}
+
+	if peerStr == "" {
+		log.Fatalln("未指定集群节点")
+	}
+	peerSplit := strings.Split(peerStr, ",")
+	peers := make(map[raft.NodeId]raft.NodeAddr, len(peerSplit))
+	for _, peerInfo := range peerSplit {
+		idAndAddr := strings.Split(peerInfo, "@")
+		peers[raft.NodeId(idAndAddr[0])] = raft.NodeAddr(idAndAddr[1])
+	}
+
+	// 启动 server
+	s := newServer(raft.NodeId(me), peers)
 	s.Start()
 }
 
